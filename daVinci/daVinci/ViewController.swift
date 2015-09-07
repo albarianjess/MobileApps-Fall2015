d7c91e41e6b954598eed1376d6968bc512f43938
Jessie <albarian.jess@gmail.com>
2015-09-06 21:02:41 -0600

    Added function to clear text after each button is pressed

--- daVinci/daVinci/ViewController.swift
+++ daVinci/daVinci/ViewController.swift
@@ -20,22 +20,37 @@ class ViewController: UIViewController {
     
     @IBAction func switchImage(sender: UIButton){
         if sender.tag==1 {
+            clearText()
             mainImage.image=UIImage(named: "happy.png")
             happyText.text="You're awesome!"
         } else if sender.tag==2 {
+            clearText()
             mainImage.image=UIImage(named: "sad.png")
             sadText.text="Don't be sad!"
         } else if sender.tag==3 {
+            clearText()
             mainImage.image=UIImage(named: "lazy.png")
             lazyText.text="Stop procrastinating!"
         } else if sender.tag==4 {
+            clearText()
             mainImage.image=UIImage(named: "adventure.png")
             adventureText.text="Go kitty go!"
         } else if sender.tag==5 {
+            clearText()
             mainImage.image=UIImage(named: "motivated.png")
             motivatedText.text="Dive into those books!"
         }
     }
+    
+    // Clears the text after each button is pressed
+    func clearText(){
+        happyText.text = ""
+        sadText.text = ""
+        lazyText.text = ""
+        adventureText.text = ""
+        motivatedText.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
