c1dcec82e153a4b9550c003648fa236495982016
Jessie <albarian.jess@gmail.com>
2015-09-29 09:55:13 -0600

    Got article view working

--- Project1/ProjectOne/AppDelegate.swift
+++ Project1/ProjectOne/AppDelegate.swift
@@ -31,6 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
+        // This refreshes your app to the beginning
+        /*
+        let vc = application.keyWindow!.rootViewController as! ViewController
+        vc.refreshUI()*/
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
