b3373dbe30645fcff7e8b55e7dbaadcbfd21f39a
Jessie <albarian.jess@gmail.com>
2015-10-17 16:33:37 -0600

    Updated lab6

--- Lab6/Lab6/ViewController.swift
+++ Lab6/Lab6/ViewController.swift
@@ -7,35 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
+
     
     
-    
-    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer,
-        shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer:
-        UIGestureRecognizer) -> Bool {
-            return true
-    }
-    
-    
-    
-    @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
-        sender.view!.transform =
-            CGAffineTransformScale(sender.view!.transform, sender.scale, sender.scale)
-        sender.scale = 1
-    }
-    
-    
-    
-    @IBAction func handleRotate(sender: UIRotationGestureRecognizer) {
-        sender.view!.transform =
-            CGAffineTransformRotate(sender.view!.transform, sender.rotation)
-        sender.rotation = 0
-    }
-    
-    
-    
+    //-----------------------------------------------
+    // Pan Gesture moves image around and decelerates
+    //-----------------------------------------------
     @IBAction func handlePan(sender: UIPanGestureRecognizer) {
         let translation = sender.translationInView(view)
         sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
@@ -51,8 +31,41 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
             finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
             UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
                 sender.view!.center = finalPoint
-            }, completion: nil)
+                }, completion: nil)
         }
+    }
+    
+    
+    
+    //------------------------
+    // Allows multiple actions
+    //------------------------
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer:
+        UIGestureRecognizer) -> Bool {
+            return true
+    }
+    
+    
+    
+    //---------------------------------------------
+    // Pinch Gesture allows user to transform image
+    //---------------------------------------------
+    @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
+        sender.view!.transform =
+            CGAffineTransformScale(sender.view!.transform, sender.scale, sender.scale)
+        sender.scale = 1
+    }
+    
+    
+    
+    //-------------------------------------------
+    // Rotate Gesture allows user to rotate image
+    //-------------------------------------------
+    @IBAction func handleRotate(sender: UIRotationGestureRecognizer) {
+        sender.view!.transform =
+            CGAffineTransformRotate(sender.view!.transform, sender.rotation)
+        sender.rotation = 0
     }
     
     
@@ -61,9 +74,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
