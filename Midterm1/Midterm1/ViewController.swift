eb175ca51a2a89ca07259dbc249bfdfdfcb30fe3
Jessie <albarian.jess@gmail.com>
2015-10-27 19:24:57 -0600

    organized folders

---
+++ Midterm1/Midterm1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Midterm1
+//
+//  Created by Jessie Albarian on 10/27/15.
+//  Copyright (c) 2015 Albarian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
