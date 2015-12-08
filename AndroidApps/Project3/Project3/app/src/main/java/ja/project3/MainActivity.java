354672d5f2b9367417beaedd707a62d2381de4b7
albarianjess <albarian.jess@gmail.com>
2015-12-08 15:32:26 -0700

    reverting

--- AndroidApps/Project3/Project3/app/src/main/java/ja/project3/MainActivity.java
+++ AndroidApps/Project3/Project3/app/src/main/java/ja/project3/MainActivity.java
@@ -3,11 +3,11 @@ package ja.project3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CatActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cat);
+        setContentView(R.layout.activity_main);
     }
 }
--- AndroidApps/Project3/Project32/app/src/main/java/ja/project3/DogActivity.java
+++ AndroidApps/Project3/Project32/app/src/main/java/ja/project3/DogActivity.java
@@ -1,9 +1,9 @@
 package ja.project3;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class DogActivity extends AppCompatActivity {
+public class DogActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
